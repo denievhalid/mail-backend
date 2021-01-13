72dd13a1e2b5108d3783ee9cb03e719f556b1fde
denievhalid <denievhalid88@mail.ru>
2021-01-13 03:46:12 +0300

    init

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,25 +1,22 @@
 import User from "../models/User";
+import asyncHandler from "express-async-handler";
 import verifyPassword from "../helpers/verifyPassword";
 import generateToken from "../helpers/generateToken";
 import { UnprocessableEntity } from "../errors";
 
-const login = async (req, res) => {
+const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user || !(await verifyPassword(password, user.password))) {
-      throw new UnprocessableEntity();
-    }
-
-    const token = generateToken({ email });
-
-    return res.status(200).json(token);
-  } catch (e) {
-    return res.sendStatus(422);
+  if (!user || !(await verifyPassword(password, user.password))) {
+    throw new UnprocessableEntity();
   }
-};
+
+  const token = generateToken({ email });
+
+  return res.status(200).json(token);
+});
 
 const register = (req, res) => {
   return res.json();
